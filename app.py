4c3c6e470fd9f60079e0eeee588275c66b406386
Hesam Haghpanah <43572130+h-haghpanah@users.noreply.github.com>
2023-10-15 06:30:34 +0330

    Update app.py

--- app.py
+++ app.py
@@ -158,4 +158,4 @@ def send_php(path):
     return send_from_directory('programs', path)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port='5000',debug=True)
+    app.run(host='0.0.0.0',port='85',debug=True)
